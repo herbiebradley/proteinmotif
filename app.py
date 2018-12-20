7c2e44d5784199c6c5d703a15c5f178a0d5a0af8
Herbie Bradley <mail@herbiebradley.com>
2018-12-20 16:30:52 +0000

    Add Bootstrap, create navbar, add biopython to requirements

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+# import the Flask class from the flask module
+from flask import Flask, render_template, url_for
+
+# create the application object
 app = Flask(__name__)
 
+# use decorators to link the function to a url
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route("/help")
+def help():
+    return render_template('help.html', title='Help')
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
-
+# start the server with the 'run()' method
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
